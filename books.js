4e043c4d681ba06539b0527d038bfdaec1e7a447
xmalachi <angelmalachi@gmail.com>
2022-05-30 22:30:53 +0100

    update

---
+++ books.js
@@ -0,0 +1,88 @@
+
+
+
+// get all elements required
+// title, author, isbn
+
+
+// add items to table
+
+function addItems(){
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let isbn = document.getElementById('isbn').value
+
+
+    let table = document.getElementsByClassName('table')[0]
+    let tbody = document.getElementsByTagName('tbody')[0]
+    let trgroup = document.createElement('tr')
+    trgroup.classList.add('bookItems')
+
+
+    let trItem = 
+    `<td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><a id="removeBtn" onclick="removeItem()"><i class="fa-solid fa-trash-can"></i></a></td>`
+    
+    console.log(trItem)
+
+    if(title && author && isbn){
+        trgroup.innerHTML =  trItem;
+    
+        tbody.appendChild(trgroup)
+    
+        alert('Book added Successfully')
+        return
+    }
+    else{
+        alert('Kindly insert all fields')
+    }
+
+}
+
+
+// remove button
+
+function removeItem(){
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let isbn = document.getElementById('isbn').value
+
+
+    let table = document.getElementsByClassName('table')[0]
+    let tbody = document.getElementsByTagName('tbody')[0]
+    let trgroup = document.createElement('tr')
+    trgroup.classList.add('bookItems')
+
+    let removeBtn =  document.getElementById('removeBtn')
+
+    removeBtn.parentElement.parentElement.remove()
+
+    // trgroup.innerHTML =  ``;
+    
+
+}
+
+
+// dark mode 
+
+function changeDarkMode(){
+    let modeToggle = document.getElementById('modeToggle')
+    let body = document.getElementById('body')
+    body.style.backgroundColor= 'black';
+    body.style.color = 'white';
+
+    let modeToggler = document.getElementsByClassName('modeToggler')[0]
+    modeToggler.innerHTML = `<button id="modeToggle" onclick="changeLightMode()"><i class="fa-solid fa-toggle-on"></i></i></button>`
+}
+
+function changeLightMode(){
+    let modeToggle = document.getElementById('modeToggle')
+    let body = document.getElementById('body')
+    body.style.backgroundColor= 'white';
+    body.style.color = 'black';
+
+    let modeToggler = document.getElementsByClassName('modeToggler')[0]
+    modeToggler.innerHTML = `<button id="modeToggle" onclick="changeDarkMode()"><i class="fa-solid fa-toggle-off"></i></i></button>`
+}
